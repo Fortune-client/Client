5529c08b6bc287706b98a41be301f3386014b385
patap <71760927+patapdev@users.noreply.github.com>
2022-02-06 19:00:51 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+class Config {
+
+	static data = {
+		maxMemory: 2048,
+		optifine: true,
+		discord: false,
+		minecraftFolder: "<use default>"
+	};
+	static file;
+
+	static init(base) {
+		Config.file = base + "/config.json";
+	}
+
+	static load() {
+		if(fs.existsSync(Config.file)) {
+			Config.data = JSON.parse(fs.readFileSync(Config.file, "UTF-8"));
+			if(!Config.data.minecraftFolder) {
+				Config.data.minecraftFolder = "<use default>";
+			}
+		}
+	}
+
+	static save() {
+		fs.writeFileSync(Config.file, JSON.stringify(Config.data));
+	}
+
+	static getGameDirectory(defaultDirectory) {
+		if(Config.data.minecraftFolder != "<use default>") {
+			return Config.data.minecraftFolder;
+		}
+
+		return defaultDirectory;
+	}
+
+}
+
+module.exports = Config;
